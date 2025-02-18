e00a00dafd44530d4bacf52fd495725919c6fdab
Selvakumar Boominathan <selvans2k@gmail.com>
2025-02-19 00:35:24 +0530

    changes implemented

--- src/components/contactForm/ContactForm.jsx
+++ src/components/contactForm/ContactForm.jsx
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
-const ContactForm = ({ contacts, setContacts }) => {
+const ContactForm = ({ contacts, addContacts }) => {
+  const initialValues = { full_name: "", phome_number: "" };
+  const [form, setForm] = useState(initialValues);
+  const onChangeInput = (e) => {
+    setForm({ ...form, [e.target.name]: [e.target.value] });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (form.full_name === "" || form.phome_number === "") {
+      alert("please Enter the data");
+    }
+    addContacts([...contacts, form]);
+  };
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <div>
-        <input name="full_name" type="text" placeholder="Full Name" />
+        <input
+          name="full_name"
+          type="text"
+          placeholder="Full Name"
+          onChange={onChangeInput}
+        />
       </div>
       <div>
         <input
           type="number"
+          onChange={onChangeInput}
           name="phone number"
           placeholder="Phone Number"
           maxLength={"10"}
